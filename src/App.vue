d53d4b85026e8d00a9f949da11d78b95c0710650
semkaway <semkaway@gmail.com>
2019-05-16 16:15:48 +0300

    [ADD] all files

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Homepage/>
+  </div>
+</template>
+
+<script>
+import Homepage from '@/components/Homepage'
+
+export default {
+  name: 'app',
+  components: { Homepage  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Homepage.vue
@@ -0,0 +1,10 @@
+<template lang="html">
+  <h1>Homepage</h1>
+</template>
+
+<script>
+
+</script>
+
+<style lang="css" scoped>
+</style>
