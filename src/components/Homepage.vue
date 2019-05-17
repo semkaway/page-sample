20b1eaf626b20de25c6108789365da76457f87c1
semkaway <semkaway@gmail.com>
2019-05-17 16:40:36 +0300

    [FIX] class naming

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,65 +1,74 @@
 <template>
   <main class="homepage">
+      <contact-form id="contactForm" hidden/>
+      <!-- <div class="homepage">
+
+      </div> -->
       <carousel class="homepage__carousel" hidden/>
       <div class="homepage__images">
           <img src="@/assets/img/main-img.png">
       </div>
       <div class="homepage__info">
-          <p class="homepage__info__paragraph">Израиль, Тель-Авив</p>
-          <p class="homepage__info__headline">Медицинский центр Анадолу</p>
-          <div class="homepage__homepage__quote__author">
-              <homepage__quote__author image="img/logo1.png" title="JCI" info="Joint Commission International"/>
-              <homepage__quote__author image="img/logo1.png" title="JCI" info="Joint Commission International"/>
-              <homepage__quote__author image="img/logo2.png" title="ESMO" info="European Society for Medical Oncology"/>
+          <p class="info__paragraph">Израиль, Тель-Авив</p>
+          <p class="info__headline">Медицинский центр Анадолу</p>
+          <div class="info-block-holder">
+              <info-block image="img/logo1.png" title="JCI" info="Joint Commission International"/>
+              <info-block image="img/logo1.png" title="JCI" info="Joint Commission International"/>
+              <info-block image="img/logo2.png" title="ESMO" info="European Society for Medical Oncology"/>
           </div>
           <div class="homepage__quote">
               <img src="@/assets/img/quotes.png" alt="quotes" class="homepage__quote__image">
               <p class="homepage__quote__text">Если тебе тяжело, значит ты поднимаешься в гору.
                   Тебе легко, значит ты летишь в пропасть. Если драгоценный камень попадает в грязь, он остается драгоценностью.
                   Если пыль подымается к небу, она остается пылью.</p>
-              <div class="homepage__quote__author">
-                  <div class="homepage__quote__author__text-holde">
-                      <div>
-                          <p class="homepage__quote__author__text-holder__text homepage__quote__author__text-holder__text--bold">Риккардо Розати</p>
-                          <p class="homepage__quote__author__text-holder__text homepage__quote__author__text-holder__text--muted">Доктор в Медицинском центре Анадолу</p>
-                      </div>
+              <div class="quote__author">
+                  <div class="quote__author__text-holder">
+                      <p class="quote__author__text-holder__text quote__author__text-holder__text--bold">Риккардо Розати</p>
+                      <p class="quote__author__text-holder__text quote__author__text-holder__text--muted">Доктор в Медицинском центре Анадолу</p>
                   </div>
-                  <div class="homepage__quote__author__image-holder">
-                      <img class="homepage__quote__author__image" src="@/assets/img/author.png">
+                  <div class="quote__author__image-holder">
+                      <img class="quote__author__image" src="@/assets/img/author.png">
                   </div>
               </div>
           </div>
       </div>
       <div class="homepage__button-holder" hidden>
-          <button type="button" class="homepage__button-holder__button">Связаться с клиникой</button>
+          <button type="button" class="homepage__button-holder__button" @click="showForm">Связаться с клиникой</button>
       </div>
   </main>
 </template>
 
 <script>
+import TheForm from '@/components/TheForm'
 import InfoBlock from '@/components/InfoBlock'
 import Carousel from '@/components/Carousel'
 
 export default {
     name: 'Homepage',
-    components: {'homepage__quote__author': InfoBlock, 'carousel': Carousel}
+    components: {'contact-form': TheForm, 'info-block': InfoBlock, 'carousel': Carousel},
+    methods: {
+        showForm() {
+            let form = document.getElementById('contactForm')
+            form.hidden = false
+        }
+    }
 }
 
 </script>
 
 <style>
 
-.homepage__quote__author {
+.quote__author {
     display: flex;
     flex-direction: row;
 }
 
-.homepage__quote__author__text-holder {
+.quote__author__text-holder {
     display: flex;
     align-items: center;
 }
 
-.homepage__quote__author__text-holder__text--bold {
+.quote__author__text-holder__text--bold {
     margin-top: 12.42px;
     font-weight: bold;
     font-size: 16px;
@@ -67,24 +76,24 @@ export default {
     color: rgba(0, 0, 0, 0.87);
 }
 
-.homepage__quote__author__text-holder__text--muted {
+.quote__author__text-holder__text--muted {
     font-size: 13px;
     line-height: 17px;
     color: rgba(0, 0, 0, 0.6);
 }
 
-.homepage__quote__author__image-holder {
+.quote__author__image-holder {
     width: 48px;
     height: 48px;
     margin-left: 25px;
 }
 
-
-.homepage__quote__author__text-holder {
+.quote__author__text-holder {
     margin-left: 0;
+    display: block;
 }
 
-.homepage__quote__author__text-holder__text {
+.quote__author__text-holder__text {
     margin: 0;
     margin-bottom: 2px;
 }
@@ -123,14 +132,14 @@ export default {
         box-sizing: border-box;
     }
 
-    .homepage__info__paragraph {
+    .info__paragraph {
         margin: 0;
         font-size: 16px;
         line-height: 19px;
         color: rgba(0, 0, 0, 0.87);
     }
 
-    .homepage__info__headline {
+    .info__headline {
         margin: 0;
         margin-top: 5px;
         font-weight: bold;
@@ -139,7 +148,11 @@ export default {
         color: rgba(0, 0, 0, 0.87);
     }
 
-    .homepage__homepage__quote__author {
+    .info-block-holder {
+        margin-top: 24px;
+    }
+
+    .homepage__quote__author {
         margin-top: 20px;
         margin-bottom: 20px;
     }
---
+++ src/components/TheForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="form__container">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TheForm"
+    }
+</script>
+
+<style>
+
+    .form__container {
+        position: absolute;
+        /* top: 0;
+        bottom: 0; */
+        border: red solid 1px;
+        height: 100vh;
+        width: 100%;
+        background-color: rgb(255, 255, 255);
+    }
+
+</style>
